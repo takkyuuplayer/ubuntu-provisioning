977439e758cce58ed138ef697d45b80631f9e995
takkyuuplayer <takkyuuplayer@gmail.com>
2015-03-27 21:03:21 +0000

    Add workspace directory

---
+++ cookbooks/development/recipes/default.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: development
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+%w(/workspace).each do |dir|
+  directory dir do
+    action :create
+    user "vagrant"
+    group "vagrant"
+    mode 0775
+  end
+end
