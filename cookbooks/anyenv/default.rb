626dbb30f8d0e19b8b53d505ff69da3e8d803f0f
takkyuuplayer <takkyuuplayer@gmail.com>
2016-01-16 18:53:27 +0900

    Add anyenv and rbenv

--- cookbooks/anyenv/default.rb
+++ cookbooks/anyenv/default.rb
@@ -1,13 +1,23 @@
 include_recipe "anyenv::system"
 
 file "/usr/local/etc/anyenvrc" do
-  action :create
   owner "root"
   group "root"
+  action :create
   mode  "0644"
   content <<EOF
 export ANYENV_ROOT="#{node["anyenv"]["anyenv_root"]}"
-export PATH="<%= @install_dir %>/bin:$PATH"
+export PATH="${ANYENV_ROOT}/bin:$PATH"
 eval "$(anyenv init -)"
+EOF
+end
+
+file "/etc/profile.d/anyenv.sh" do
+  owner "root"
+  group "root"
+  action :create
+  mode "0644"
+  content <<EOF
+source "/usr/local/etc/anyenvrc"
 EOF
 end
