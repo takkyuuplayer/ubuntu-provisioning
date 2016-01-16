687139543dc3099ebde864a2814526575bdf19f7
takkyuuplayer <takkyuuplayer@gmail.com>
2016-01-16 23:04:27 +0900

    Add common role

--- roles/sakura.rb
+++ roles/sakura.rb
@@ -1,8 +1,3 @@
-include_recipe "../cookbooks/essential/apt.rb"
-include_recipe "../cookbooks/essential/swap.rb"
-include_recipe "../cookbooks/essential/default.rb"
-include_recipe "../cookbooks/essential/build-essential.rb"
-
 include_recipe "../cookbooks/security/ssh.rb"
 
 include_recipe "../cookbooks/anyenv/default.rb"
--- roles/vagrant.rb
+++ roles/vagrant.rb
@@ -1,9 +1,3 @@
-include_recipe "../cookbooks/essential/apt.rb"
-include_recipe "../cookbooks/essential/swap.rb"
-include_recipe "../cookbooks/essential/default.rb"
-include_recipe "../cookbooks/essential/build-essential.rb"
-include_recipe "../cookbooks/essential/directories.rb"
-
 include_recipe "../cookbooks/anyenv/default.rb"
 include_recipe "../cookbooks/anyenv/rbenv.rb"
 include_recipe "../cookbooks/anyenv/plenv.rb"
