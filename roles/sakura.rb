1bd5850168f451ea736ed1ec8501f72ffb818e42
takkyuuplayer <takkyuuplayer@gmail.com>
2016-01-16 20:43:54 +0000

    Add homepage

--- roles/sakura.rb
+++ roles/sakura.rb
@@ -2,3 +2,8 @@ include_recipe "../cookbooks/security/ssh.rb"
 
 include_recipe "../cookbooks/anyenv/default.rb"
 include_recipe "../cookbooks/anyenv/rbenv.rb"
+
+include_recipe "../cookbooks/nginx/default.rb"
+include_recipe "../cookbooks/nginx/proxy.rb"
+
+include_recipe "../cookbooks/supervisor/default.rb"
