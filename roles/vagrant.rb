961cf6ad33c331881ae9324cbbf33d75d25e5e35
takkyuuplayer <takkyuuplayer@gmail.com>
2016-01-16 20:34:53 +0900

    Add directories

--- roles/vagrant.rb
+++ roles/vagrant.rb
@@ -2,6 +2,7 @@ include_recipe "../cookbooks/essential/apt.rb"
 include_recipe "../cookbooks/essential/swap.rb"
 include_recipe "../cookbooks/essential/default.rb"
 include_recipe "../cookbooks/essential/build-essential.rb"
+include_recipe "../cookbooks/essential/directories.rb"
 
 include_recipe "../cookbooks/anyenv/default.rb"
 include_recipe "../cookbooks/anyenv/rbenv.rb"
