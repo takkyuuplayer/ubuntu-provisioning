2e52f3f0bb39268a50d7066c6579af64e423b5b3
takkyuuplayer <takkyuuplayer@gmail.com>
2016-02-09 15:12:52 +0900

    Add dist-upgrade command

--- cookbooks/essential/apt.rb
+++ cookbooks/essential/apt.rb
@@ -1,8 +1,14 @@
+execute 'apt-get update' do
+  command 'apt-get update -y'
+end
+
 execute 'apt-get-upgrade' do
   action :nothing
   command 'apt-get upgrade -y'
 end
 
-execute 'apt-get update' do
-  command 'apt-get update -y'
+execute 'apt-get-dist-upgrade' do
+  action :nothing
+  command 'apt-get dist-upgrade -y'
 end
+
