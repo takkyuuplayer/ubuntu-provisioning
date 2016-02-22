3d18e4cb1d678a178675a74c44d2ae2e86333cdb
takkyuuplayer <takkyuuplayer@gmail.com>
2016-02-23 01:34:24 +0900

    Add zabbix.conf.php

--- cookbooks/monitor/apache2.rb
+++ cookbooks/monitor/apache2.rb
@@ -15,6 +15,41 @@ file "/etc/apache2/ports.conf" do
   notifies :restart, 'service[apache2]'
 end
 
+file "/etc/php5/apache2/php.ini" do
+  action :edit
+  block do |content|
+    content.gsub!(";date.timezone =", "date.timezone = Asia/Tokyo")
+  end
+  notifies :restart, 'service[apache2]'
+end
+
+file "/usr/share/zabbix/conf/zabbix.conf.php" do
+  owner 'root'
+  group 'root'
+  mode  '644'
+  content <<-PHP
+<?php
+// Zabbix GUI configuration file.
+global $DB;
+
+$DB['TYPE']     = 'MYSQL';
+$DB['SERVER']   = 'localhost';
+$DB['PORT']     = '0';
+$DB['DATABASE'] = 'zabbix';
+$DB['USER']     = 'root';
+$DB['PASSWORD'] = '';
+
+// Schema name. Used for IBM DB2 and PostgreSQL.
+$DB['SCHEMA'] = '';
+
+$ZBX_SERVER      = 'localhost';
+$ZBX_SERVER_PORT = '10051';
+$ZBX_SERVER_NAME = '';
+
+$IMAGE_FORMAT_DEFAULT = IMAGE_FORMAT_PNG;
+  PHP
+end
+
 service 'apache2' do
   action [:enable, :start]
 end
