90813dfc9cb3662e29b368579239e98ec7c4652e
takkyuuplayer <takkyuuplayer@gmail.com>
2016-02-23 01:16:19 +0900

    Add apt-update, upgrade, dist-upgrade with cache

--- cookbooks/essential/apt.rb
+++ cookbooks/essential/apt.rb
@@ -1,14 +1,17 @@
+yyyymmdd = Time.now.strftime("%Y%m%d")
+
 execute 'apt-get update' do
-  command 'apt-get update -y'
+  command "apt-get update -y && touch /tmp/apt-get-update-#{yyyymmdd}"
+  not_if "test -e /tmp/apt-get-update-#{yyyymmdd}"
 end
 
 execute 'apt-get-upgrade' do
-  action :nothing
-  command 'apt-get upgrade -y'
+  command "apt-get upgrade -y && touch /tmp/apt-get-upgrade-#{yyyymmdd}"
+  not_if "test -e /tmp/apt-get-upgrade-#{yyyymmdd}"
 end
 
 execute 'apt-get-dist-upgrade' do
-  action :nothing
-  command 'apt-get dist-upgrade -y'
+  command "apt-get upgrade -y && touch /tmp/apt-get-dist-upgrade-#{yyyymmdd}"
+  not_if "test -e /tmp/apt-get-dist-upgrade-#{yyyymmdd}"
 end
 
