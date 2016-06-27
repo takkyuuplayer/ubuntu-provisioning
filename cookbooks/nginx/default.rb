4c087d073680167f7a29830504e58551b613284a
takkyuuplayer <takkyuuplayer@gmail.com>
2016-06-27 20:07:57 +0900

    Install the latest nginx

--- cookbooks/nginx/default.rb
+++ cookbooks/nginx/default.rb
@@ -1,9 +1,18 @@
-package 'nginx' do
-  action :install
+execute 'add-apt-repository ppa:nginx/stable' do
+  command <<-CMD
+    add-apt-repository ppa:nginx/stable <<EOF &&
+EOF
+    apt-get update -y
+  CMD
+  not_if 'ls -al /etc/apt/sources.list.d/ | grep nginx'
 end
 
 service 'nginx' do
   action [:enable, :start]
+end
+
+package 'nginx' do
+  action :install
 end
 
 %w(/web).each do |dir|
@@ -23,7 +32,6 @@ end
   mode "775"
   end
 end
-
 
 directory "/var/cache/nginx" do
   action :create
