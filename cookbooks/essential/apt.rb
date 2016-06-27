63ebce49ee1ed92487a9d030141b41960bc3d4f2
takkyuuplayer <takkyuuplayer@gmail.com>
2016-06-27 21:46:59 +0900

    Fix comment

--- cookbooks/essential/apt.rb
+++ cookbooks/essential/apt.rb
@@ -9,13 +9,13 @@ execute 'apt-get update' do
   not_if "test -e /tmp/apt-get-update-#{yyyymmdd}"
 end
 
-execute 'apt-get-upgrade' do
+execute 'apt-get upgrade' do
   command "apt-get upgrade -y && touch /tmp/apt-get-upgrade-#{yyyymmdd}"
   not_if "test -e /tmp/apt-get-upgrade-#{yyyymmdd}"
 end
 
-execute 'apt-get-dist-upgrade' do
-  command "apt-get upgrade -y && touch /tmp/apt-get-dist-upgrade-#{yyyymmdd}"
+execute 'apt-get dist-upgrade' do
+  command "apt-get dist-upgrade -y && touch /tmp/apt-get-dist-upgrade-#{yyyymmdd}"
   not_if "test -e /tmp/apt-get-dist-upgrade-#{yyyymmdd}"
 end
 
