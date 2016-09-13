da8850b768d42e321636ebd96ffd9f1736ed37b8
takkyuuplayer <takkyuuplayer@gmail.com>
2016-09-13 15:27:25 +0900

    Install jenkins

---
+++ cookbooks/jenkins.rb
@@ -0,0 +1,16 @@
+execute 'apt-key add' do
+  command <<-CMD
+    wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | apt-key add - ;
+    echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list;
+    sudo apt-get update -y
+  CMD
+  not_if '-f /etc/apt/sources.list.d/jenkins.list'
+end
+
+%w(
+jenkins
+).each do |pkg|
+  package pkg do
+    action :install
+  end
+end
