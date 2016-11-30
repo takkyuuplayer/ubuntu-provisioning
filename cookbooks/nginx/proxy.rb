94342ffa7a4683bf09873b5bf6bebbea36029b0a
takkyuuplayer <takkyuuplayer@gmail.com>
2016-12-01 02:00:21 +0900

    Fix nginx setting

--- cookbooks/nginx/proxy.rb
+++ cookbooks/nginx/proxy.rb
@@ -7,22 +7,28 @@ directory "/web/#{node["proxy"][:app_name]}" do
     mode "775"
 end
 
+link "/etc/nginx/sites-enabled/#{node[:proxy][:app_name]}-proxy.vh" do
+  action :create
+  to "/etc/nginx/sites-available/#{node[:proxy][:app_name]}-proxy.vh"
+end
+
 template "/etc/nginx/sites-available/#{node[:proxy][:app_name]}-proxy.vh" do
+  ssl = node[:proxy][:ssl] \
+    && File.exists?(node[:proxy][:ssl][:ssl_certificate]) \
+    && File.exists?(node[:proxy][:ssl][:ssl_certificate_key]) \
+    ? node[:proxy][:ssl] : nil
+
   source "templates/proxy.vh.erb"
   mode "644"
   owner 'root'
   group 'root'
   action :create
   notifies :restart, 'service[nginx]'
+  puts ssl
   variables({
     :app_name => node[:proxy][:app_name],
     :fqdn     => node[:proxy][:fqdn],
     :port     => node[:proxy][:port],
-    :ssl      => node[:proxy][:ssl],
+    :ssl      => ssl,
   })
-end
-
-link "/etc/nginx/sites-enabled/#{node[:proxy][:app_name]}-proxy.vh" do
-  action :create
-  to "/etc/nginx/sites-available/#{node[:proxy][:app_name]}-proxy.vh"
 end
