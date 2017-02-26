763d2a88c3d831b2129599c73af93173782bfccb
takkyuuplayer <takkyuuplayer@gmail.com>
2017-02-27 00:56:24 +0900

    Replace with virtual_hosts for sakura

--- roles/sakura.rb
+++ roles/sakura.rb
@@ -5,6 +5,6 @@ include_recipe "../cookbooks/anyenv/rbenv.rb"
 
 include_recipe "../cookbooks/letsencrypt/default.rb"
 include_recipe "../cookbooks/nginx/default.rb"
-include_recipe "../cookbooks/nginx/proxy.rb"
+include_recipe "../cookbooks/nginx/virtualhosts.rb"
 
 include_recipe "../cookbooks/monitor/mackerel.rb"
