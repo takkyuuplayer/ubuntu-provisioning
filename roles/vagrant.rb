927b6f98e7cc9bbfc7125f67417d55856bc79495
takkyuuplayer <takkyuuplayer@gmail.com>
2017-04-18 22:24:42 +0900

    Add awscli

--- roles/vagrant.rb
+++ roles/vagrant.rb
@@ -2,6 +2,7 @@ include_recipe "../cookbooks/anyenv/default.rb"
 include_recipe "../cookbooks/anyenv/rbenv.rb"
 include_recipe "../cookbooks/anyenv/plenv.rb"
 include_recipe "../cookbooks/anyenv/ndenv.rb"
+include_recipe "../cookbooks/anyenv/pyenv.rb"
 
 include_recipe "../cookbooks/samba/default.rb"
 
