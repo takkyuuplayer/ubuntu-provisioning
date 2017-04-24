84f6525a643a0b6e80e5237a205c01559246f625
takkyuuplayer <takkyuuplayer@gmail.com>
2017-04-24 23:27:42 +0900

    Add docker-compose

--- cookbooks/docker/default.rb
+++ cookbooks/docker/default.rb
@@ -18,9 +18,14 @@ execute 'apt-key add' do
   not_if 'test -f /etc/apt/sources.list.d/docker.list'
 end
 
-package 'docker-ce' do
-  action :install
+%w(
+docker-ce
+).each do |pkg|
+  package pkg do
+    action :install
+  end
 end
+
 
 service 'docker' do
   action [:enable, :start]
@@ -29,4 +34,12 @@ end
 execute "sudo usermod -aG docker #{node[:user][:name]}" do
   command "sudo usermod -aG docker #{node[:user][:name]}"
   not_if "groups #{node[:user][:name]} | grep docker"
+end
+
+execute "Install docker-compose" do
+  command <<-CMD
+    curl -L https://github.com/docker/compose/releases/download/#{node[:docker][:compose][:version]}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+  CMD
+  not_if "which docker-compose && docker-compose --version | grep '#{node[:docker][:compose][:version]}'"
 end
