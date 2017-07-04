861cbf90f6f10b3799ecbcea0e63e03182734c9c
takkyuuplayer <takkyuuplayer@gmail.com>
2017-07-04 23:12:13 +0900

    Add renewal crontab

--- cookbooks/letsencrypt/default.rb
+++ cookbooks/letsencrypt/default.rb
@@ -2,12 +2,16 @@ package 'letsencrypt' do
   action :install
 end
 
-execute 'crontab: letsencrypt renew --agree-tos' do
-  command <<-CMD
-  crontab -l > /tmp/crontab
-  echo "15 3,15 * * * /usr/bin/letsencrypt renew -n --agree-tos" >> /tmp/crontab
-  crontab /tmp/crontab
-  rm /tmp/crontab
-  CMD
-  not_if 'crontab -l | grep "letsencrypt renew"'
+file "/etc/cron.d/renew-letsencrypt-certification" do
+  owner 'root'
+  group 'root'
+  mode  '644'
+  content <<-CRONTAB
+SHELL=/bin/bash
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+MAILTO=root
+HOME=/
+
+15 3,15 * * * /usr/bin/letsencrypt renew -n --agree-tos
+CRONTAB
 end
