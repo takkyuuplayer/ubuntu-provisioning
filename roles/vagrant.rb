e270cfec35946ab5834527f3f0d2b947bb96ce74
takkyuuplayer <takkyuuplayer@gmail.com>
2017-09-25 05:37:30 +0900

    Install golang

--- roles/vagrant.rb
+++ roles/vagrant.rb
@@ -1,5 +1,7 @@
 include_recipe "../cookbooks/essential/swap.rb"
 include_recipe "../cookbooks/essential/ntp.rb"
 
+include_recipe "../cookbooks/anyenv/default.rb"
+
 include_recipe "../cookbooks/samba/default.rb"
 include_recipe "../cookbooks/docker/default.rb"
