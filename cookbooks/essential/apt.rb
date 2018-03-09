563b7a98fd5311139b154d61e37cab8ba83e9eb0
Takafumi <t.sekiguchi@sproutatwork.com>
2018-03-08 23:51:37 -0500

    Fix essential not to upgrade

--- cookbooks/essential/apt.rb
+++ cookbooks/essential/apt.rb
@@ -1,22 +1,20 @@
 yyyymmdd = Time.now.strftime("%Y%m%d")
 
-execute 'dpkg --configure -a' do
-  command "dpkg --configure -a"
-end
-
 execute 'apt-get update' do
   command "apt-get update -y && touch /tmp/apt-get-update-#{yyyymmdd}"
   not_if "test -e /tmp/apt-get-update-#{yyyymmdd}"
 end
 
-execute 'apt-get upgrade' do
-  command "apt-get upgrade -y && touch /tmp/apt-get-upgrade-#{yyyymmdd}"
-  not_if "test -e /tmp/apt-get-upgrade-#{yyyymmdd}"
-end
+if node[:essential][:upgrade] then
+  execute 'apt-get upgrade' do
+    command "apt-get upgrade -y && touch /tmp/apt-get-upgrade-#{yyyymmdd}"
+    not_if "test -e /tmp/apt-get-upgrade-#{yyyymmdd}"
+  end
 
-execute 'apt-get dist-upgrade' do
-  command "apt-get dist-upgrade -y && touch /tmp/apt-get-dist-upgrade-#{yyyymmdd}"
-  not_if "test -e /tmp/apt-get-dist-upgrade-#{yyyymmdd}"
+  execute 'apt-get dist-upgrade' do
+    command "apt-get dist-upgrade -y && touch /tmp/apt-get-dist-upgrade-#{yyyymmdd}"
+    not_if "test -e /tmp/apt-get-dist-upgrade-#{yyyymmdd}"
+  end
 end
 
 execute 'apt autoremove' do
