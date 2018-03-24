0ea01075963381c6fae863acd23716d8f4a22bf8
takkyuuplayer <takkyuuplayer@gmail.com>
2018-03-24 04:25:35 -0400

    Fixes

--- cookbooks/nginx/ssl.rb
+++ cookbooks/nginx/ssl.rb
@@ -1,19 +1,19 @@
-directory File.dirname(node[:nginx][:ssl][:key]) do
+directory File.dirname(node[:nginx][:ssl][:key_path]) do
   action :create
   mode   "0700"
 end
 
-remote_file node[:nginx][:ssl][:key] do
+remote_file node[:nginx][:ssl][:key_path] do
   source "files/server.key"
   action :create
 end
 
-directory File.dirname(node[:nginx][:ssl][:crt]) do
+directory File.dirname(node[:nginx][:ssl][:crt_path]) do
   action :create
   mode   "0700"
 end
 
-remote_file node[:nginx][:ssl][:crt] do
+remote_file node[:nginx][:ssl][:crt_path] do
   source "files/server.crt"
   action :create
 end
